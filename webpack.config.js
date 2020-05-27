6ff15640de40ed3e9f09399a9040057946a8139d
Freedy <21914103@zju.edu.cn>
2020-05-27 15:17:02 +0800

    引入css

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,28 @@
 var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
     mode: 'development',
     entry: './src/index.js',
+    devtool: 'inline-source-map',
+    devServer: {
+        contentBase: './dist'
+    },
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: '[name].[contenthash].js'
-    }
+    },
+    plugins: [new HtmlWebpackPlugin({
+        title: 'My App',
+        template: 'src/assets/index.html'
+    })],
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+        ],
+    },
 };
+
